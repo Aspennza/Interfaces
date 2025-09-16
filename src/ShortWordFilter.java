94a4ca7a0c6c9cf90361f99f4f9e6e496864ec51
Zoe Aspenns <aspennza@mail.uc.edu>
2025-09-16 17:32:50 -0400

    ShortWordFilter draft complete

---
+++ src/ShortWordFilter.java
@@ -0,0 +1,16 @@
+public class ShortWordFilter implements Filter
+{
+    @Override
+    public boolean accept(Object x)
+    {
+        String s = (String) x;
+        boolean ret = false;
+
+        if(s.length() < 5)
+        {
+            ret = true;
+        }
+
+        return ret;
+    }
+}
