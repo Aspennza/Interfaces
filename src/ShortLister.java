bea8ca7312ff6767e1de7d6daf038c0864a33343
Zoe Aspenns <aspennza@mail.uc.edu>
2025-09-16 17:51:01 -0400

    Created necessary code for JFileChooser in ShortLister; added a loop to read the file

--- src/ShortLister.java
+++ src/ShortLister.java
@@ -1,14 +1,63 @@
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+//Tasks:
+//Create UML diagrams
+//Write javadoc
 
 public class ShortLister
 {
     public static void main(String[] args)
     {
+        JFileChooser chooser = new JFileChooser();
+
+        File selectedFile;
+
+        String rec = "";
+
+        ArrayList<String> lines = new ArrayList<>();
+
+        String[] fields;
+
 
         try
         {
+            System.out.println("Please select a file to be filtered for short words.");
 
+            File workingDirectory = new File(System.getProperty("user.dir"));
+            chooser.setCurrentDirectory(workingDirectory);
+
+            if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+            {
+                selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+
+                InputStream in =
+                        new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(in));
+
+                while(reader.ready())
+                {
+                    rec = reader.readLine();
+                    lines.add(rec);
+                }
+                reader.close();
+                System.out.println("\n\nThe data file has been read.\n");
+
+                System.out.println("Short words from the file:");
+                System.out.println("==========================");
+
+                for(String l : lines)
+                {
+                    fields = l.split(" ");
+                }
+            }
         }
         catch (FileNotFoundException e)
         {
