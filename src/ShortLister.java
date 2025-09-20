0d31b4e057d9efed6508753da479315f164f5126
Zoe Aspenns <aspennza@mail.uc.edu>
2025-09-20 11:49:30 -0400

    JavaDoc for ShortLister complete

--- src/ShortLister.java
+++ src/ShortLister.java
@@ -7,33 +7,48 @@ import java.util.ArrayList;
 import static java.nio.file.StandardOpenOption.CREATE;
 
 //Tasks:
-//check prof. wulf's second recording on thursday for reference on ShortLister and BigRectLister
 //Create UML diagrams
 //Write javadoc
 
+/**
+ * Implements the ShortWordFilter Object to filter a given text file and output any words less than 5 characters long.
+ * @author Zoe Aspenns aspennza@mail.uc.edu
+ */
 public class ShortLister
 {
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args)
     {
+        //This file chooser is used to select a file to read
         JFileChooser chooser = new JFileChooser();
 
+        //This File holds the file selected by the user
         File selectedFile;
 
+        //This Object contains a method for accepting or rejecting words based on their length
         ShortWordFilter filter = new ShortWordFilter();
 
+        //This String holds the line that has just been read from the selected file
         String rec = "";
 
+        //This ArrayList holds all the lines that have been read from the selected file
         ArrayList<String> lines = new ArrayList<>();
 
+        //This array holds all the words from the file after they have been split up
         String[] fields;
 
+        //This algorithm reads the file selected by the user and tries to catch exceptions
         try
         {
             System.out.println("Please select a file to be filtered for short words.");
 
+            //This File holds the current directory
             File workingDirectory = new File(System.getProperty("user.dir"));
             chooser.setCurrentDirectory(workingDirectory);
 
+            //This algorithm tests whether the user has selected a file and either closes the program or sets up a file reader depending on the result
             if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
             {
                 selectedFile = chooser.getSelectedFile();
@@ -44,6 +59,7 @@ public class ShortLister
                 BufferedReader reader =
                         new BufferedReader(new InputStreamReader(in));
 
+                //This algorithm reads each line from the chosen file into a String and adds that String to the lines ArrayList
                 while(reader.ready())
                 {
                     rec = reader.readLine();
@@ -55,16 +71,22 @@ public class ShortLister
                 System.out.println("Short words from the file:");
                 System.out.println("==========================");
 
+                //This algorithm splits each line from the file into individual words, then filters those words by length and prints words below 5 characters to the console
                 for(String l : lines)
                 {
+                    //This int tracks the current index for the fields array as it is looped through
                     int fieldsIndex = 0;
+
+                    //This boolean tracks whether each word is longer or shorter than 5 characters and stores a true/false value
                     boolean shortWord = false;
                     fields = l.split(" ");
 
+                    //This algorithm checks if each word in the fields array is longer or shorter than 5 characters and prints the words below 5 characters long
                     for(String f : fields)
                     {
                         shortWord = filter.accept(fields[fieldsIndex].replaceAll("[.,;?:!()/]", ""));
 
+                        //This algorithm checks that a word is below 5 characters and prints it to the console
                         if(shortWord)
                         {
                             System.out.println(fields[fieldsIndex].replaceAll("[.,;?:!()/]", ""));
