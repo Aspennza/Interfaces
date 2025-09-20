9872972fdfd9592d1060a5987621f47ba7700905
Zoe Aspenns <aspennza@mail.uc.edu>
2025-09-20 12:42:15 -0400

    JavaDoc for ShortWordFilter and BigRectLister complete

--- src/BigRectLister.java
+++ src/BigRectLister.java
@@ -2,25 +2,43 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * Randomly generates Rectangle Objects, determines whether their perimeters are above or below 10, and outputs any Rectangles with perimeters above 10.
+ * @author Zoe Aspenns aspennza@mail.uc.edu
+ */
 public class BigRectLister
 {
+    //This int sets the total number of Rectangles to be generated
     static final int TOTAL_RECTANGLES = 10;
+
+    //This Random object allows for random generation of height and width parameters
     static Random gen = new Random();
+
+    //This ArrayList stores all the Rectangles created
     static ArrayList<Rectangle> rectangles = new ArrayList<>();
+
+    //This BigRectangleFilter allows the Rectangles to be tested for perimeters above or below 10
     static BigRectangleFilter filter;
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args)
     {
+        //This int counts how many Rectangles have perimeters above 10
         int counter = 0;
         createDataSet();
 
+        //This BigRectangleFilter allows the Rectangles to be tested for perimeters above or below 10
         filter = new BigRectangleFilter();
 
         System.out.println("Rectangle Number          Height Width  Perimeter");
         System.out.println("=================================================");
 
+        //This algorithm loops through all the Rectangles in the rectangles ArrayList, checks if they have perimeters above or below 10, and prints those above 10
         for(Rectangle rec : rectangles)
         {
+            //This algorithm tests if each Rectangle in the rectangles ArrayList has a perimeter above or below 10 and prints the data from those above 10
             if(filter.accept(rec))
             {
                 counter++;
@@ -30,6 +48,9 @@ public class BigRectLister
         System.out.println("\n" + counter + " accepted out of " + rectangles.size());
     }
 
+    /**
+     * This method uses randomly generated numbers to create a dataset of 10 Rectangle Objects.
+     */
     private static void createDataSet()
     {
         int height = 0;
--- src/ShortWordFilter.java
+++ src/ShortWordFilter.java
@@ -1,5 +1,15 @@
+/**
+ * Allows the creation of ShortWordFilter objects that come with a method for checking
+ * the length of a String and returning a true or false value based on its length.
+ * @author Zoe Aspenns aspennza@mail.uc.edu
+ */
 public class ShortWordFilter implements Filter
 {
+    /**
+     * Returns a true or false value after judging whether a String input is above or below 5 characters.
+     * @param x The Object (presumably a String) whose length will be tested.
+     * @return  A boolean representing whether the String is above or below 5 characters.
+     */
     @Override
     public boolean accept(Object x)
     {
